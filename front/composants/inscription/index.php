6ebc7be65fa4b9ac943a489e1db327f72834cd02
anastasiia19c <156657920+anastasiia19c@users.noreply.github.com>
2024-12-20 14:08:33 +0100

    designe pour la page d'inscription & responsive

--- front/composants/inscription/index.php
+++ front/composants/inscription/index.php
@@ -25,8 +25,8 @@
             <label for="email">Adresse e-mail :</label>
             <input type="email" id="email" placeholder="Adresse e-mail" required>
 
-            <label for="pseudo">Mot de passe :</label>
-            <input type="text" id="pseudo" placeholder="Mot de passe" required>
+            <label for="pseudo">Pseudo :</label>
+            <input type="text" id="pseudo" placeholder="Pseudo" required>
 
             <label for="password">Mot de passe :</label>
             <input type="password" id="password" placeholder="Mot de passe" required>
