d585b9581581a4272007758cca824c1a419ed321
Nina <nina.gmz115@gmail.com>
2024-12-20 11:40:57 +0100

    page chanson

---
+++ front/composants/chanson/indexChanson.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styleChanson.css"></link>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <title>Chanson</title>
+</head>
+<body>
+    <header>
+    </header>
+
+    <main>
+        <div>
+            <h2>Poster une chanson</h2>
+            <form>
+                <div>
+                    <label for="file">Poster une chanson de Noël :</label>
+                    <input id="file" type="file" accept="audio/*,video/*">
+                </div>
+                <div>
+                    <button type="submit">Poster</button>
+                </div>
+            </form>
+
+        </div>
+    </main>
+
+    <footer>
+        <div>
+            <h3>Hackathon 2024</h3>
+        </div>
+    </footer>
+</body>
+</html>
+
+<?php
